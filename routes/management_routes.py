74d6a3df609a1fdae522fc780681f4cd600d781c
Davi Fonseca Viana <daviviana2602@gmail.com>
2026-04-24 19:47:40 -0300

    rewrite management_routes

--- routes/management_routes.py
+++ routes/management_routes.py
@@ -1,10 +1,12 @@
 # Rotas para administração do app
 
+from services.management_services import promover_usuarios_services, rebaixar_usuario_services, reativar_usuario_services, desativar_usuario_services
+
 from sqlalchemy.orm import Session
 
 from dependencies import get_db, checar_admin
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 
 from models import UserTable
 
@@ -12,172 +14,49 @@ from schemas import AlterationUserSchema
 
 from response_schemas import CommonResponse
 
-from helpers import resposta_sucesso
-
-
 
 management_router = APIRouter(prefix = "/management", tags=["management"])   # define o caminho = domínio/management/(rota esolhinha)
 
 
 
 @management_router.patch("/promover_usuario", response_model=CommonResponse)
-async def promover_usuario(
-                    promote_user: AlterationUserSchema,
-                    db: Session = Depends(get_db),
-                    admin: UserTable = Depends(checar_admin)    # checando se é admin e quem é pelo id
-                    ):
+def promover_usuario(
+        promote_user: AlterationUserSchema,
+        db: Session = Depends(get_db),
+        admin: UserTable = Depends(checar_admin)    # checando se é admin e quem é pelo id
+        ):
     
-    
-    # Procura o usuário a ser promovido
-    usuario_a_promover = db.query(UserTable).filter_by(
-        id=promote_user.usuario_a_sofrer_alteracao
-        ).first()
-
-    
-    if not usuario_a_promover:
-        raise HTTPException(status_code=404, detail="Usuário não encontrado")   # usuário inexistente retorna erro
-    
-    if not usuario_a_promover.ativo:
-        raise HTTPException(status_code=400, detail="usuário está desativado e não pode ser promovido")
-    
-    if usuario_a_promover.admin == True:
-        raise HTTPException(status_code=403, detail="O usuário escolhido já é um administrador")
-    
-    usuario_a_promover.admin = True
-    
-    try:
-        db.commit()
-        db.refresh(usuario_a_promover)
-    except Exception:
-        db.rollback()
-        raise
-    
-    
-    return resposta_sucesso(            # success já vem como True pela função
-        f"Usuário {usuario_a_promover.nome} agora é admin",    
-        {
-            "id": usuario_a_promover.id,
-        }
-    )
+    return promover_usuarios_services(promote_user, db, admin)
         
         
         
 @management_router.patch("/rebaixar_usuario", response_model=CommonResponse)
-async def rebaixar_usuario(
+def rebaixar_usuario(
                     demote_user: AlterationUserSchema,
                     db: Session = Depends(get_db),
                     admin: UserTable = Depends(checar_admin)    # checando se é admin e quem é pelo id
                     ):
     
-    
-    # Procura o usuário a ser rebaixado
-    usuario_a_rebaixar = db.query(UserTable).filter_by(
-        id=demote_user.usuario_a_sofrer_alteracao
-        ).first()
-    
-    
-    if not usuario_a_rebaixar:
-        raise HTTPException(status_code=404, detail="Usuário não encontrado")   
-    
-    if usuario_a_rebaixar.admin == False:
-        raise HTTPException(status_code=403, detail="O usuário escolhido não é administrador")
-    
-    if usuario_a_rebaixar.id == admin.id:
-        raise HTTPException(status_code=403, detail="Você não pode se rebaixar")    # checagem pra não deixar se rebaixar sozinho
-    
-    
-    usuario_a_rebaixar.admin = False
-    
-    try:
-        db.commit()
-        db.refresh(usuario_a_rebaixar)
-    except Exception:
-        db.rollback()
-        raise
-    
-    
-    return resposta_sucesso(            # success já vem como True pela função
-        f"Usuário {usuario_a_rebaixar.nome} agora não é mais admin",    
-        {
-            "id": usuario_a_rebaixar.id,
-        }
-    )
+    return rebaixar_usuario_services(demote_user, db, admin)
    
     
     
 @management_router.patch("/desativar_usuario", response_model=CommonResponse)
-async def desativar_usuario(
-    delete_user_id: AlterationUserSchema,
-    db: Session = Depends(get_db),
-    admin: UserTable = Depends(checar_admin)    # checando se é admin e quem é pelo id
-):
+def desativar_usuario(
+        delete_user_id: AlterationUserSchema,
+        db: Session = Depends(get_db),
+        admin: UserTable = Depends(checar_admin)    # checando se é admin e quem é pelo id
+        ):
     
-    # Procura o usuário a ser desativado
-    usuario = db.query(UserTable).filter_by(
-        id=delete_user_id.usuario_a_sofrer_alteracao
-    ).first()
-    
-    
-    if not usuario:
-        raise HTTPException(status_code=404, detail="Usuário não encontrado")
-    
-    if usuario.admin:
-        raise HTTPException(status_code=403, detail="O usuário escolhido é administrador e não pode ser desativado")
-    
-    if not usuario.ativo:
-        raise HTTPException(status_code=400, detail="Usuário já está desativado")   # contas "banidas"/desativadas
-    
-    
-    usuario.ativo = False   # desativando usuário
-    
-    try:
-        db.commit()
-        db.refresh(usuario)
-    except Exception:
-        db.rollback()
-        raise
-    
-    
-    return resposta_sucesso(            # success já vem como True pela função
-        f"Usuário {usuario.nome} foi desativado",    
-        {
-            "id": usuario.id,
-        }
-    )
+    return desativar_usuario_services(delete_user_id, db, admin)
    
     
     
 @management_router.patch("/reativar_usuario", response_model=CommonResponse)
-async def reativar_usuario(
+def reativar_usuario(
     reactive_user_id: AlterationUserSchema,
     db: Session = Depends(get_db),
     admin: UserTable = Depends(checar_admin)    # checando se é admin e quem é pelo id
-):
+    ):
     
-    # Procura o usuário a ser reativado
-    usuario = db.query(UserTable).filter_by(
-        id=reactive_user_id.usuario_a_sofrer_alteracao
-        ).first()
-    
-    if not usuario:
-        raise HTTPException(status_code=404, detail="Usuário não encontrado")
-    
-    if usuario.ativo:
-        raise HTTPException(status_code=400, detail="Usuário já está ativo")
-    
-    usuario.ativo = True    # reativando o usuário
-    
-    try:
-        db.commit()
-        db.refresh(usuario)
-    except Exception:
-        db.rollback()
-        raise
-    
-    
-    return resposta_sucesso(            # success já vem como True pela função
-        f"Usuário {usuario.nome} foi reativado",    
-        {
-            "id": usuario.id,
-        }
-    )
+    return reativar_usuario_services(reactive_user_id, db, admin)
