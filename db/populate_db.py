e7e806837d8f391be94f24ae05004f7aa4cc75cd
Davi Fonseca Viana <daviviana2602@gmail.com>
2026-05-09 14:16:39 -0300

    fix: popular_cardapio (prices)

--- db/populate_db.py
+++ db/populate_db.py
@@ -130,31 +130,36 @@ ITENS_INICIAIS = [
 ]
 
 
-def popular_cardapio():    
-    db = SessionLocal()  
+def popular_cardapio():
+    db = SessionLocal()
+
     try:
         for item in ITENS_INICIAIS:
-            
-            # verifica se o item já existe, pra não duplicar
+
+            # verifica se o item já existe
             exists = db.query(CardapioTable).filter_by(
                 nome=item["nome"]
-                ).first()
-            
+            ).first()
+
             if not exists:
+
                 novo_item = CardapioTable(
                     nome=item["nome"],
                     categoria=item["categoria"],
-                    preco=item["preco"]
+
+                    preco_tradicional=item["preco_tradicional"],
+                    preco_pequeno=item["preco_pequeno"],
+                    preco_grande=item["preco_grande"]
                 )
-                
-                db.add(novo_item)   
-                
+
+                db.add(novo_item)
+
         db.commit()
         print("Cardápio populado com sucesso!")
-        
+
     except Exception as e:
         db.rollback()
         print("Erro ao popular cardápio:", e)
-        
+
     finally:
-        db.close()  
+        db.close()
